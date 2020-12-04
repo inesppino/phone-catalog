0cfa5b2a86866003afb8dfaa207c555cdeb3407e
Ines Pedraza <ines.pedraza@ultimasoftware.com>
2020-12-04 22:18:29 +0100

    fix: catalog styles and added long description in database

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -10,26 +10,36 @@ function Catalog() {
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
       const response = await getPhones();
-      response ? setPhones(response) : setMessage('Ha ocurrido un error');
-      console.log(phones)
-      setIsLoading(false);
+      setPhones(response.data);
+    } catch (ex) {
+      console.log(ex);
+      setMessage('Ha ocurrido un error');
     }
-    fetchData();
+    setIsLoading(false);
+  };
+  
+  useEffect(() => {
+    async function populateComponent() {
+      await fetchData();
+    }
+    populateComponent();
   }, []);
 
   return (
     <main>
       <LoadingMask loading={isLoading} text={"loading..."} className="h-100 w-100">
-        <div className="container">
-          <h1 className="mb-4 h1 mb-4 text-center">Phone Catalog</h1>
+        <div className="container d-flex flex-column">
+          <h1 className="h1 p-4 text-center">Phone Catalog</h1>
           {message && <p>{message}</p>}
           <ul className="catalog-list p-0">
             {phones.map((phone) => (
-              <Card key={phone.id} phone={phone} />
+              <li key={phone.id}>
+                <Card phone={phone} />
+              </li>
             ))}
           </ul>
         </div>
