eeab4ef0cafd979a816ce0c00e1eec95aa255791
Ines Pedraza <ines.pedraza@ultimasoftware.com>
2020-12-04 20:24:51 +0100

    feat(catalog): added third-party for loader component

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import "../assets/css/catalog.css";
 import { getPhones } from "../services/fakePhoneService";
+import LoadingMask from "react-loadingmask";
 import Card from "./Card";
+import "../assets/css/catalog.css";
+import "react-loadingmask/dist/react-loadingmask.css";
 
 function Catalog() {
   const [phones, setPhones] = useState([]);
@@ -12,18 +14,17 @@ function Catalog() {
     async function fetchData() {
       setIsLoading(true);
       const response = await getPhones();
-      response ? setPhones(response.data) : setMessage('Ha ocurrido un error');
+      response ? setPhones(response) : setMessage('Ha ocurrido un error');
+      console.log(phones)
       setIsLoading(false);
     }
     fetchData();
   }, []);
 
   return (
-    <>
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <main className="container">
+    <main>
+      <LoadingMask loading={isLoading} text={"loading..."} className="h-100 w-100">
+        <div className="container">
           <h1 className="mb-4 h1 mb-4 text-center">Phone Catalog</h1>
           {message && <p>{message}</p>}
           <ul className="catalog-list p-0">
@@ -31,9 +32,9 @@ function Catalog() {
               <Card key={phone.id} phone={phone} />
             ))}
           </ul>
-        </main>
-      )}
-    </>
+        </div>
+      </LoadingMask>
+    </main>
   );
 }
 
